ddda1c51ab24b2dbca89b4348809740f68a05553
empmaynmtusrgdyt <leammax@yandex.ru>
2025-01-12 17:25:16 +0300

    second commit

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -7,10 +7,13 @@ public class GuessTheNumber {
                 "Enter 1 to play as the number picker; enter 2 to play as the number guesser " +
                 "(the computer will pick).");
         Scanner input = new Scanner(System.in);
-        int choose_a_mode = input.nextInt();
-        while (choose_a_mode > 3){
-            System.out.println("Incorrect input. Please enter either 1 or 2.");
+        int choose_a_mode;
+        while (true){
             choose_a_mode = input.nextInt();
+            if (choose_a_mode == 1 || choose_a_mode == 2){
+                break;
+            }
+            System.out.println("Incorrect input. Please enter either 1 or 2.");
         }
         if (choose_a_mode == 1){
             System.out.println("You’ve decided to pick the number. Okay, enter your number from 1 to 100:");
@@ -18,8 +21,24 @@ public class GuessTheNumber {
         }
         else if (choose_a_mode == 2){
             System.out.println("You’ve decided to guess the number. Enter your guess:");
-            Random r = new Random(101);
+            Random r = new Random();
+            int computer_number = r.nextInt(101);
             int user_guess = input.nextInt();
+            while (true){
+                if (user_guess < computer_number){
+                    System.out.println("The secret number is larger than your input. " +
+                            "Please enter another guess:");
+                    user_guess = input.nextInt();
+                } else if (user_guess > computer_number) {
+                    System.out.println("The secret number is smaller than your input. " +
+                            "Please enter another guess:");
+                    user_guess = input.nextInt();
+                } else {
+                    System.out.println("Congratulations! You guessed correctly! The number was - "
+                            + computer_number);
+                    break;
+                }
+            }
         }
     }
 }
